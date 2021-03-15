26f9d696350ff4784c4404737d7a51e0534b619b
Robret Fraser <robert.fraser876@gmail.com>
2021-03-15 16:43:55 -0700

    3rd

--- main_app/models.py
+++ main_app/models.py
@@ -1,15 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Product:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, brand, description, yr):
-    self.name = name
-    self.brand = brand
-    self.description = description
-    self.yr = yr
-
-products = [
-  Product('iphone x', 'apple', 'smart phone', 2021),
-  Product('smart tv', 'LG', '55 inch flatscreen with smart hub', 2019),
-  Product('wireless mouse', 'Philips', 'wireless mouse with usb dongle', 2020)
-]
