38e6077120e89ee0245c276566c32c6da0151be6
didin21 <tanashikovadina@gmail.com>
2021-03-16 01:23:43 -0700

    adds user register/login functionality

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+# import your models here
+from .models import Product, Profile
 
-# Register your models here.
+# Register your models here
+admin.site.register(Product)
+admin.site.register(Profile)
