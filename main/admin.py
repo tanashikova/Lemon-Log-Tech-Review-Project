053f820f16b2354e4b9b5bdbf52d9dc3879d9c0d
didin21 <tanashikovadina@gmail.com>
2021-03-17 11:49:46 -0700

    adds reviews page

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Product, Review
+from .models import Review
 
 # Register your models here
-admin.site.register(Product)
 admin.site.register(Review)
--- main/models.py
+++ main/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Product(models.Model):
-    name = models.CharField(max_length=50)
-    brand = models.CharField(max_length=50)
-    description = models.TextField(max_length=250)
-    manufactoring_year = models.IntegerField()
-
 class Review(models.Model):
     title = models.CharField(max_length=50)
     product = models.CharField(max_length=35)
