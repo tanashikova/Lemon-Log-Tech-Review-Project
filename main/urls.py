d639bfa7bca2c26c74be6ee404719745f9ed8dc9
Robret Fraser <robert.fraser876@gmail.com>
2021-03-17 16:17:11 -0700

    reviews are on home page

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
      path('', views.home, name='home'),
      path('about/', views.about, name='about'),
-     path('reviews/', views.reviews_index, name='index'),
-     path(r'reviews/<int:review_id>/', views.reviews_detail, name="review_detail")
+     path('index/', views.reviews_index, name='index'),
+     path('reviews/<int:review_id>/', views.reviews_detail, name="review_detail")
 ]
